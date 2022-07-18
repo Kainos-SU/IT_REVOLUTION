f58b9fc63ffae42697052562ca883780cebeefce
Маркус <marcus.petrihen@gmail.com>
2022-07-18 18:49:53 +0300

    logaut and delete routes

--- server/controllers/userlist.js
+++ server/controllers/userlist.js
@@ -1,19 +1,18 @@
 const User = require("../models/User");
+const Token = require("../models/Token");
 const bcrypt = require("bcryptjs");
 
 module.exports.getByUserList = async function (req, res) {
   try {
     console.log("Сервер getByUserList");
 
-    console.log(req.query);
-
     const limitUser = req.query.limit_user ? req.query.limit_user : 5; // Скільки користувачів на сторінку
-    const page = req.query.page ? req.query.page : 1; // Сторінка яку нада відобразити
+    const page = req.query.page ? req.query.page : 0; // Сторінка яку нада відобразити
 
-    const currentPage = page; // Нинішня сторінка
+    let currentPage = page; // Нинішня сторінка
 
     const countUser = await User.countDocuments({}).exec(); // Максимальна кількість сторінок
-    const maxPage = Math.ceil(countUser / limitUser); // Округлення сторінок в більшу сторону.
+    const maxPage = Math.ceil(countUser / limitUser); // Округлення сторінок в більшу сторону. 3.02 >> 4
 
     if (currentPage > maxPage) {
       return res.status(423).json({ message: "Не корректний запит" });
@@ -33,6 +32,10 @@ module.exports.getByUserList = async function (req, res) {
       };
     });
 
+    if (currentPage === 0) {
+      currentPage = 1;
+    }
+
     res.status(200).json({ userList, currentPage, maxPage });
   } catch (error) {
     console.log(error);
@@ -42,7 +45,7 @@ module.exports.getByUserList = async function (req, res) {
 
 module.exports.getByUserId = async function (req, res) {
   try {
-    console.log("Сервер getByUserListId");
+    console.log("Сервер getByUserId");
 
     const candidateUser = await User.findById(req.params.id);
 
@@ -64,22 +67,33 @@ module.exports.getByUserId = async function (req, res) {
 module.exports.userDelete = async function (req, res) {
   try {
     console.log("Сервер delete");
-    console.log(req.params.id);
-    console.log(req.body);
+
+    // req.params.id // Користувача якого мають видалити.
+    // req.body // Користувач який хоче видалити іншого користувача. "АДМІН"
 
     const adminUser = await User.findById(req.body._id); // Користувач який хоче видалити іншого користувача.
+
+    const countLoginDeleteUser = await Token.find({ _id_user: req.params.id });
 
     const passwordResult = bcrypt.compareSync(
       req.body.password,
       adminUser.password
     ); // Чи правильний пароль у адміна
 
-    console.log(passwordResult);
-    console.log(adminUser);
-
     if (passwordResult === true && adminUser.admin === true) {
-      await User.remove({ _id: req.params.id }); // Delete user in DB // Користувач якого буде видалено.
-      res.status(200).json({ message: "Користувача успішно видалено" });
+      if (req.body._id !== req.params.id) {
+        await User.remove({ _id: req.params.id }); // Delete user in DB // Користувач якого буде видалено.
+        for (let idx = 0; idx <= countLoginDeleteUser.length; idx++) {
+          await Token.findOneAndDelete({
+            _id_user: req.params.id,
+          });
+        }
+        res.status(200).json({ message: "Користувача успішно видалено" });
+      } else if (req.body._id === req.params.id) {
+        res.status(423).json({
+          message: "Ви не можете видалити самі себе.",
+        });
+      }
     } else {
       res.status(423).json({
         message:
