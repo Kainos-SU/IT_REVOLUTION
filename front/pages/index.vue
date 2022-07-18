a14e2a0b4f0df0ab1c7a563596386c764513d9f2
Kainos-SU <kapinos4@gmail.com>
2022-07-18 06:08:58 +0300

    Fix of new name

--- front/pages/index.vue
+++ front/pages/index.vue
@@ -1,24 +1,24 @@
 <template lang="pug">
-  v-container(fluid='' fill-height='')
-    v-layout(align-center='' justify-center='')
-      v-flex(xs12='' sm8='' md4='')
-        v-card.mx-auto(max-width='1200')
-          v-toolbar(color='indigo' dark='')
-            v-toolbar-title Список усіх користувачів
-          v-list
-            .text-center
-              v-progress-circular(v-if="isLoading" :size="30" color="black" indeterminate)
-            v-list-item(v-for='item in items' :key='item._id' @click="$router.push(`/user/${item._id}`)")
-              v-list-item-icon
-                v-icon(color='pink' v-show="item.admin")
-                  | mdi-crown
-              v-list-item-content
-                v-list-item-title(v-text='item.email')
-              v-list-item-content
-                v-list-item-title(v-text='item.name')
-              v-list-item-avatar
-                v-avatar(color="indigo")
-                  v-icon(dark) mdi-account-circle
+v-container(fluid='' fill-height='')
+  v-layout(align-center='' justify-center='')
+    v-flex(xs12='' sm8='' md4='')
+      v-card.mx-auto(max-width='1200')
+        v-toolbar(color='indigo' dark='')
+          v-toolbar-title Список усіх користувачів
+        v-list
+          .text-center
+            v-progress-circular(v-if="isLoading" :size="30" color="black" indeterminate)
+          v-list-item(v-for='item in items' :key='item._id' @click="$router.push(`/user/${item._id}`)")
+            v-list-item-icon
+              v-icon(color='pink' v-show="item.admin")
+                | mdi-crown
+            v-list-item-content
+              v-list-item-title(v-text='item.email')
+            v-list-item-content
+              v-list-item-title(v-text='item.name')
+            v-list-item-avatar
+              v-avatar(color="indigo")
+                v-icon(dark) mdi-account-circle
 </template>
 
 <script>
