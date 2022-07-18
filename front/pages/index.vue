2240119a32d7cd1d4fafa1d5d89bae4b7fa5a76c
Kainos-SU <kapinos4@gmail.com>
2022-07-18 19:04:44 +0300

    add simple pagination

--- front/pages/index.vue
+++ front/pages/index.vue
@@ -2,13 +2,14 @@
 v-container(fluid='' fill-height='')
   v-layout(align-center='' justify-center='')
     v-flex(xs12='' sm8='' md4='')
+      span {{total}}
       v-card.mx-auto(max-width='1200')
         v-toolbar(color='indigo' dark='')
           v-toolbar-title Список усіх користувачів
         v-list
           .text-center
             v-progress-circular(v-if="isLoading" :size="30" color="black" indeterminate)
-          v-list-item(v-for='item in items' :key='item._id' @click="$router.push(`/user/${item._id}`)")
+          v-list-item(v-if="!isLoading && !error" v-for='item in items' :key='item._id' @click="$router.push(`/user/${item._id}`)")
             v-list-item-icon
               v-icon(color='pink' v-show="item.admin")
                 | mdi-crown
@@ -19,6 +20,7 @@ v-container(fluid='' fill-height='')
             v-list-item-avatar
               v-avatar(color="indigo")
                 v-icon(dark) mdi-account-circle
+      v-pagination(:length="total" @input="fetchUsers" v-model="page")
 </template>
 
 <script>
@@ -31,18 +33,23 @@ export default {
       isLoading: true,
       error: false,
       items: [],
+      total: 0,
+      page: 1
     }
   },
   mounted() {
-    this.fetchUsers()
+    this.fetchUsers(this.page)
   },
   methods: {
     ...mapActions({ getUsers: 'user/getUsers', deleteUser: 'user/deleteUser' }),
-    async fetchUsers() {
+    async fetchUsers(pageNumber) {
+      this.isLoading = true;
       try {
-        const response = await this.getUsers()
+        const response = await this.getUsers(pageNumber - 1)
         if (response.status === 200) {
+	  this.page = pageNumber
           this.items = response.data.userList
+	  this.total = response.data.maxPage
         }
       } catch (err) {
         console.log(err)
--- front/store/user/actions.js
+++ front/store/user/actions.js
@@ -1,8 +1,8 @@
 import config from '~/config/index'
 
 export default {
-  getUsers({ rootState }) {
-    return this.$axios.get(`${config.apiPath}/userlist`)
+  getUsers({ rootState }, pageNumber) {
+    return this.$axios.get(`${config.apiPath}/userlist?page=${pageNumber}`)
   },
   getUserById({ rootState }, id) {
     return this.$axios.get(`${config.apiPath}/userlist/${id}`)
